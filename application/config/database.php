83632884c535ff542cd66f1a6a16b7c214bf5daa
rivai izari <rivaiizari@gmail.com>
2019-05-20 05:52:29 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,8 +75,8 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'dbizariazure.database.windows.net',
-	'username' => 'dbizariazure',
+	'hostname' => 'dbizariazur.database.windows.net',
+	'username' => 'dbizariazur',
 	'password' => '14051995Dp',
 	'database' => 'web3',
 	//'dbdriver' => 'mysqli',
