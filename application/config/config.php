3758cefe503861204ca62a7e0f5cb3da7a28c90b
rivai izari <rivaiizari@gmail.com>
2019-05-20 05:54:02 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/cicrud-ajax';
-$config['base_url'] = 'https://izariwebapptes1.azurewebsites.net';
+$config['base_url'] = 'https://izariwebapp.azurewebsites.net';
 /*
 $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 $config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
